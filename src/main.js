7662f4eba56cb24b0050025a51069feb9bf118fe
Zack Jones <zajonesck83@gmail.com>
2023-07-10 07:35:50 -0500

    theme defenition delete

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 
 // Vuetify
 import "vuetify/styles";
-import { createVuetify, ThemeDefinition } from "vuetify";
+import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
