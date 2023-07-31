347681603e2c74c75f403cb49e05ec6f6df0d88e
Zack Jones <zajonesck83@gmail.com>
2023-07-30 20:45:25 -0500

    deleted a period

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
           <v-col>
             <v-card>
               <v-card-title class="text-center headline"
-                >Select the type of weight and bar you're using.
+                >Select the type of weight and bar you're using
               </v-card-title>
 
               <v-select
